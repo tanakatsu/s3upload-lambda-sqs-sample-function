950590e61e54eedef5f8080e275606e0ae9280a1
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2016-08-01 22:06:08 +0900

    AccessKeyId, SecretAccessKey from ENV

--- lambda-config.js
+++ lambda-config.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 console.log('role=', process.env.LAMBDA_ROLE);
 
 module.exports = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'ap-northeast-1',
   handler: 'CreateThumbsWithMessaging.handler', // 実行したいファイル名と同じにする
   role: process.env.LAMBDA_ROLE,
